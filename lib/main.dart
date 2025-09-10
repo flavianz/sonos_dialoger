227f7e5ce3f1944342b60823c9fc3a975fd7e465
Flavian Züllig <flavian.zuellig@stud.unibas.ch>
2025-09-10 21:24:22 +0200

    add auth operations

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,35 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_localizations/firebase_ui_localizations.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:sonos_dialoger/router.dart';
 
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseFirestore.instance.settings = Settings(
+    persistenceEnabled: true,
+    cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
   );
+
+  FirebaseUIAuth.configureProviders([
+    EmailAuthProvider(),
+    GoogleProvider(
+      clientId:
+          "378625854578-o7t4pvcol1pf90sskp8e7n75jn7vv8gt.apps.googleusercontent.com",
+    ),
+  ]);
+
   runApp(const MyApp());
 }
 
@@ -24,17 +42,17 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final activeRouter = router;
-  //late final StreamSubscription<User?> _authSub;
+  late final StreamSubscription<User?> _authSub;
 
   @override
   void initState() {
     super.initState();
 
-    /*_authSub = FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    _authSub = FirebaseAuth.instance.authStateChanges().listen((User? user) {
       if (user == null) {
-        activeRouter.go("/sign-in");
+        activeRouter.go("/auth");
       }
-    });*/
+    });
 
     SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(
@@ -46,13 +64,24 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void dispose() {
-    //_authSub.cancel();
+    _authSub.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      localizationsDelegates: [
+        // Creates an instance of FirebaseUILocalizationDelegate with overridden labels
+        FirebaseUILocalizations.withDefaultOverrides(const DeLocalizations()),
+
+        // Delegates below take care of built-in flutter widgets
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+
+        // This delegate is required to provide the labels that are not overridden by LabelOverrides
+        FirebaseUILocalizations.delegate,
+      ],
       routerConfig: activeRouter,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
--- lib/router.dart
+++ lib/router.dart
@@ -1,7 +1,22 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
   routes: [
-
+    GoRoute(
+      path: "/auth",
+      builder: (context, state) {
+        return SignInScreen(
+          actions: [
+            AuthStateChangeAction<SignedIn>((context, state) {
+              context.go('/');
+            }),
+            AuthStateChangeAction<UserCreated>((context, state) {
+              context.go('/');
+            }),
+          ],
+        );
+      },
+    ),
   ],
 );

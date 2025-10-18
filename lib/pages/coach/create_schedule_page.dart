ce512323c437c5ed4b78246bb9663984a39e4727
Flavian Zuellig <flavianzuellig@hotmail.com>
2025-10-18 21:02:25 +0200

    Add CreateSchedulePage widget

---
+++ lib/pages/coach/create_schedule_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateSchedulePage extends StatefulWidget {
+  const CreateSchedulePage({super.key});
+
+  @override
+  State<CreateSchedulePage> createState() => _CreateSchedulePageState();
+}
+
+class _CreateSchedulePageState extends State<CreateSchedulePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

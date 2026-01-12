3008d28e42eca561a0d4587b54c4bbbda2328e9c
Flavian Züllig <flavianzuellig@hotmail.com>
2026-01-12 16:14:51 +0100

    auto redirect from homescreen to first page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:sonos_dialoger/providers/firestore_providers/user_providers.dart';
+import 'package:flutter/scheduler.dart';
+
+import '../core/user.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, ref) {
-    final data = ref.watch(userDataProvider).value;
-    return Center(child: Text("${data?.first ?? ""} ${data?.last ?? ""}"));
+    final data = ref.watch(userDataProvider);
+    if (data.isLoading) {
+      return Center(child: CircularProgressIndicator());
+    }
+    if (data.hasError) {
+      return Center(child: Text("Ups, hier hat etwas nicht geklappt"));
+    }
+    final userData = data.value;
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      if (userData == null) {
+        context.go("/auth");
+      } else if (userData.role == UserRole.admin) {
+        context.go("/admin/payments");
+      } else if (userData.role == UserRole.coach ||
+          userData.role == UserRole.dialog) {
+        context.go("/dialoger/payments");
+      } else {
+        // should never be the case
+        context.go("/auth");
+      }
+    });
+    return Center(child: CircularProgressIndicator());
   }
 }

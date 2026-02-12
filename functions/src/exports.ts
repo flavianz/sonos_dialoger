8a3032e3d533d6f4c9a11b6e59140690bd3dd7d2
Flavian Züllig <flavianzuellig@hotmail.com>
2026-02-12 11:11:58 +0100

    fix bug with non-padded time in exports

--- functions/src/exports.ts
+++ functions/src/exports.ts
@@ -153,7 +153,7 @@ function buildSheet(
         );
         sheet.addRow({
             date: `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`,
-            time: `${date.getUTCHours()}:${date.getUTCMinutes()}`,
+            time: `${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`,
             dialoger: dialogers[data["dialoger"]] ?? "Unbekannt",
             location: locations[data["location"]] ?? "Unbekannt",
             amount: amount,
